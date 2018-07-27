79f0903c609e21693646ebbdf83203e88053de04
bruno <bruno.kedzierski@wp.pl>
2018-07-27 16:37:56 +0200

    *UPDATE*

---
+++ src/Zadanie4/Zadanie4.java
@@ -0,0 +1,25 @@
+package Zadanie4;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class Zadanie4 {
+
+    public static void main(String[] args) {
+        Connection connect = Jsoup.connect("http://www.tvn24.pl/");
+        try{
+            Document document = connect.get();
+            Elements links = document.select("span.title");
+            for (Element elem : links) {
+                System.out.println(elem.text());
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
